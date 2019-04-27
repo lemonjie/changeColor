2fd663211385b055bce6a81dcf3bb2d25b454e52
lemonjie <wallowing0321@gmail.com>
2019-04-27 19:25:50 +0800

    waiting for run

--- app/src/main/java/com/example/changecolor/MainActivity.java
+++ app/src/main/java/com/example/changecolor/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.changecolor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Color;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txvRed, txvGreen, txvBlue, txvShow;    //txv = TeXtView
+    String text = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txvRed = findViewById(R.id.textViewRed);
+        txvGreen = findViewById(R.id.textViewGreen);
+        txvBlue = findViewById(R.id.textViewBlue);
+        txvShow = findViewById(R.id.textViewShow);
     }
+
+    public void changeColors(View v){
+        Random ran = new Random();
+
+        int red = ran.nextInt(256);
+        txvRed.setText("Red : "+ red);
+        txvRed.setTextColor(Color.argb(150, red, 0, 0));
+
+        int green = ran.nextInt(256);
+        txvRed.setText("Green : "+ green);
+        txvRed.setTextColor(Color.argb(150, 0, green, 0));
+
+        int blue = ran.nextInt(256);
+        txvRed.setText("Blue : "+ blue);
+        txvRed.setTextColor(Color.argb(150, 0, 0, blue));
+
+        text += red + ", " + green + ", " + blue + ", " + "\n";
+        txvShow.setTextSize(16);
+        txvShow.setText(text);
+        txvShow.setMovementMethod(new ScrollingMovementMethod());
+        txvShow.setBackgroundColor(Color.rgb(red,green,blue));
+
+    }
+
 }
